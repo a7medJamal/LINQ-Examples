788fef283b20944dced2ec90c052f47fe91c916d
Ahmed Jamal <Ahmed Jamal@LAPTOP-CQULRIFS>
2018-10-02 23:34:15 +0300

    before linq code

---
+++ LINQ Examples/LINQ Examples.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LINQ_Examples
+{
+    partial class LINQ_Examples
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.fileList = new System.Windows.Forms.ListBox();
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Location = new System.Drawing.Point(405, 382);
+            this.btnGO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(100, 28);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 393);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(117, 382);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(260, 23);
+            this.txtPass.TabIndex = 2;
+            // 
+            // fileList
+            // 
+            this.fileList.FormattingEnabled = true;
+            this.fileList.ItemHeight = 16;
+            this.fileList.Location = new System.Drawing.Point(69, 41);
+            this.fileList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(407, 324);
+            this.fileList.TabIndex = 3;
+            // 
+            // btnFiles
+            // 
+            this.btnFiles.Location = new System.Drawing.Point(69, 5);
+            this.btnFiles.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFiles.Name = "btnFiles";
+            this.btnFiles.Size = new System.Drawing.Size(100, 28);
+            this.btnFiles.TabIndex = 4;
+            this.btnFiles.Text = "Fill File List";
+            this.btnFiles.UseVisualStyleBackColor = true;
+            this.btnFiles.Click += new System.EventHandler(this.btnFiles_Click);
+            // 
+            // LINQ_Examples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 451);
+            this.Controls.Add(this.btnFiles);
+            this.Controls.Add(this.fileList);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGO);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LINQ_Examples";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "LINQ_Examples";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.ListBox fileList;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+        private System.Windows.Forms.Button btnFiles;
+    }
+}
---
+++ LINQ Examples/LINQ Examples.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_Examples
+{
+    public partial class LINQ_Examples : Form
+    {
+        public LINQ_Examples()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+         if(  FBD.ShowDialog()==DialogResult.OK)
+            {
+                txtPass.Text = FBD.SelectedPath;
+            }
+        }
+
+        private void btnFiles_Click(object sender, EventArgs e)
+        {
+            if(txtPass.Text !="")
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("Selected Path Firestly");
+            }
+        }
+    }
+}
